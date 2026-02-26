2b57c7b69cfd758e7b668b129027a8c7ff802f68
Johannsen <jogiosten@mein.gmx>
2026-02-26 23:17:09 +0100

    air gangbang

--- common/defines/JFU_defines.lua
+++ common/defines/JFU_defines.lua
@@ -107,7 +107,7 @@ NDefines.NSupply.RAILWAY_CONVERSION_COOLDOWN = 5
 
 NDefines.NAir.FUEL_COST_MULT = 0.2 -- vanilla 0.35 fuel multiplier for all air missions
 NDefines.NAir.AIR_DEPLOYMENT_DAYS = 0
-NDefines.NAir.COMBAT_DAMAGE_SCALE = 0.25 -- Higher value = more shot down planes
+NDefines.NAir.COMBAT_DAMAGE_SCALE = 0.3 -- Higher value = more shot down planes
 NDefines.NAir.MISSION_COMMAND_POWER_COSTS = {  -- command power cost per plane to create a mission
 	0.0, -- AIR_SUPERIORITY
 	0.0, -- CAS
